ac344250d5a6301f51e3edd24f3da8595bc25fd4
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-02 13:02:53 -0400

    `events.Event` class

--- events.py
+++ events.py
@@ -7,4 +7,20 @@ class Event(object):
         self.data = data
         self.windowRef = windowRef
 
-    def isKeypress()
+    def isKeypress(self, key=None):
+        if self.kind != 'key_event/PRESS':
+            return False
+        targetKey = self.getKeyCode()
+        return self.data['key'] == targetKey
+
+    def isKeyrelease(self, key=None):
+        if self.kind != 'key_event/RELEASE':
+            return False
+        targetKey = self.getKeyCode()
+        return self.data['key'] == targetKey
+
+    def getKeyCode(self, keyname):
+        return getattr(
+            self.windowRef.keys,
+            keyname.upper(),
+        )
