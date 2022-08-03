e53a2acdd6d7b64a51a6f1a6a42bd15d6631a933
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-03 13:06:56 -0400

    tkinter thing works :)

--- main.py
+++ main.py
@@ -1,118 +1,50 @@
-# Kaidun (by HktOverload)
+import time
 
-import numpy as np
-import moderngl_window as mglw
-import moderngl
+from cmu_112_graphics import *
 
-import events
-import scenes
+import mglw_main
 
-def triNormal(tri):
-    a1 = tri[3] - tri[0]
-    a2 = tri[4] - tri[1]
-    a3 = tri[5] - tri[2]
-    b1 = tri[6] - tri[0]
-    b2 = tri[7] - tri[1]
-    b3 = tri[8] - tri[2]
-    return (
-        (a2*b3) - (a3*b2),
-        (a3*b1) - (a1*b3),
-        (a1*b2) - (a2*b1),
+def mglwRootFrameCallback(app):
+    app.mglwRedrawTime = time.time()
+
+def mglwRootLoadHook(app, wincls):
+    wincls.frame_callback = \
+        lambda: \
+            mglwRootFrameCallback(app)
+
+def startMGLWRoot(app, maxTimeWithoutGLRoot=0.25):
+    app.mglwRedrawTime = time.time()
+    app.maxTimeWithoutGLRoot = maxTimeWithoutGLRoot
+    mglw_main.main(
+        load_hook=(
+            lambda wincls: \
+                mglwRootLoadHook(app, wincls)
+        ),
     )
 
-class GameWindow(mglw.WindowConfig):
-    gl_version = (3, 3)
-    window_size = (3840//3, 2160//3)
+def appStarted(app):
+    startMGLWRoot(app)
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        # Setup gamedata and scene
-        self.gamedata = dict()
-        self.scene = scenes.CubeScene
-        self.geometryState = None
-        # OpenGL / Shader code
-        with open('world_vertex.glsl', 'r') as f:
-            vertex_shader = f.read()
-        with open('world_fragment.glsl', 'r') as f:
-            fragment_shader = f.read()  
-        self.prog = self.ctx.program(
-            vertex_shader=vertex_shader,
-            fragment_shader=fragment_shader,
-        )
-        # OpenGL / Vertex data
-        self.vertBuf = self.ctx.buffer(
-            np.zeros((216,), dtype='f4')
-        )
-        self.vao = self.ctx.vertex_array(
-            self.prog,
-            [
-                (self.vertBuf, '3f 3f', 'vert', 'aux_surf_normal')
-            ]
-        )
-        # Camera
-        zeroVec3 = (0.0, 0.0, 0.0)
-        self.gamedata.update({
-            'cam_ctr': (0.0, 1.0, -2.0),
-            'd_cam_ctr': zeroVec3,
-            'cam_rot': zeroVec3,
-            'd_cam_rot': zeroVec3,
-        })
-        self.prog['cam_near'].value = 1.0
-        self.prog['cam_dist'].value = 50.0
-        # Lighting
-        self.prog['lighting_ambient'].value = 0.2
-        self.prog['lighting_maxsc'].value = 1.15
-        self.prog['lighting_light_ctr'] = (2.0, 2.0, 2.0)
-        self.prog['lighting_light_brightness'] = 50.0
-        # Lighting / Surface info
-        self.prog['surf_albedo'].value = 0.09
-        self.prog['surf_roughness'].value = 0.1
-        # Distance fog
-        self.fog_color = (0.2, 0.2, 0.2)
-        self.prog['fog_color'].value = self.fog_color
-        self.prog['fog_attenuation_coef'].value = 1.9
-    
-    def render(self, *_):
-        self.setupShaderInvocation()
-        self.ctx.clear(*self.fog_color)
-        self.ctx.enable(moderngl.DEPTH_TEST)
-        self.vao.render()
-        self.frame()
+def showGL(app):
+    try:
+        app._hideRootWindow()
+    except:
+        pass
 
-    def key_event(self, key, action, *_):
-        if action == self.wnd.keys.ACTION_PRESS:
-            kind = 'key_event/PRESS'
-        elif action == self.wnd.keys.ACTION_RELEASE:
-            kind = 'key_event/RELEASE'
-        controller = self.scene.getController(self.gamedata)
-        controller.handle(
-            self.gamedata,
-            events.Event(
-                kind,
-                {
-                    'key': key,
-                },
-                self.wnd,
-            ),
-        )
+def checkNoGLRoot(app):
+    delta = time.time() - app.mglwRedrawTime
+    if delta > app.maxTimeWithoutGLRoot:
+        print('***** KILLING MAIN TK ROOT *****')
+        app.quit()
 
-    def setupShaderInvocation(self):
-        controller = self.scene.getController(self.gamedata)
-        updates = controller.shaderUpdates(self.gamedata)
-        for k, v in updates.items():
-            self.prog[k].value = v
-        newGeometryState = self.scene.geometryState(self.gamedata)
-        if self.geometryState != newGeometryState:
-            self.vertBuf.write(
-                self.scene.buildGeometry(newGeometryState)
-            )
-            self.geometryState = newGeometryState
+def timerFired(app):
+    checkNoGLRoot(app)
 
-    def frame(self):
-        self.scene.getController().frame(self.gamedata)
-
-def main():
-    mglw.run_window_config(GameWindow, args=('--window', 'tk'))
+def redrawAll(app, *_):
+    showGL(app)
 
 if __name__ == '__main__':
-    main()
+    runApp(
+        width=3840//3, height=2160//3,
+        title='Game Window',
+    )
---
+++ mglw_main.py
@@ -0,0 +1,124 @@
+# Kaidun (by HktOverload)
+
+import numpy as np
+import moderngl_window as mglw
+import moderngl
+
+import events
+import scenes
+
+def triNormal(tri):
+    a1 = tri[3] - tri[0]
+    a2 = tri[4] - tri[1]
+    a3 = tri[5] - tri[2]
+    b1 = tri[6] - tri[0]
+    b2 = tri[7] - tri[1]
+    b3 = tri[8] - tri[2]
+    return (
+        (a2*b3) - (a3*b2),
+        (a3*b1) - (a1*b3),
+        (a1*b2) - (a2*b1),
+    )
+
+class GameWindow(mglw.WindowConfig):
+    gl_version = (3, 3)
+    window_size = (3840//3, 2160//3)
+
+    frame_callback = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Setup gamedata and scene
+        self.gamedata = dict()
+        self.scene = scenes.CubeScene
+        self.geometryState = None
+        # OpenGL / Shader code
+        with open('world_vertex.glsl', 'r') as f:
+            vertex_shader = f.read()
+        with open('world_fragment.glsl', 'r') as f:
+            fragment_shader = f.read()  
+        self.prog = self.ctx.program(
+            vertex_shader=vertex_shader,
+            fragment_shader=fragment_shader,
+        )
+        # OpenGL / Vertex data
+        self.vertBuf = self.ctx.buffer(
+            np.zeros((216,), dtype='f4')
+        )
+        self.vao = self.ctx.vertex_array(
+            self.prog,
+            [
+                (self.vertBuf, '3f 3f', 'vert', 'aux_surf_normal')
+            ]
+        )
+        # Camera
+        zeroVec3 = (0.0, 0.0, 0.0)
+        self.gamedata.update({
+            'cam_ctr': (0.0, 1.0, -2.0),
+            'd_cam_ctr': zeroVec3,
+            'cam_rot': zeroVec3,
+            'd_cam_rot': zeroVec3,
+        })
+        self.prog['cam_near'].value = 1.0
+        self.prog['cam_dist'].value = 50.0
+        # Lighting
+        self.prog['lighting_ambient'].value = 0.2
+        self.prog['lighting_maxsc'].value = 1.15
+        self.prog['lighting_light_ctr'] = (2.0, 2.0, 2.0)
+        self.prog['lighting_light_brightness'] = 50.0
+        # Lighting / Surface info
+        self.prog['surf_albedo'].value = 0.09
+        self.prog['surf_roughness'].value = 0.1
+        # Distance fog
+        self.fog_color = (0.2, 0.2, 0.2)
+        self.prog['fog_color'].value = self.fog_color
+        self.prog['fog_attenuation_coef'].value = 1.9
+    
+    def render(self, *_):
+        self.setupShaderInvocation()
+        self.ctx.clear(*self.fog_color)
+        self.ctx.enable(moderngl.DEPTH_TEST)
+        self.vao.render()
+        self.frame()
+
+    def key_event(self, key, action, *_):
+        if action == self.wnd.keys.ACTION_PRESS:
+            kind = 'key_event/PRESS'
+        elif action == self.wnd.keys.ACTION_RELEASE:
+            kind = 'key_event/RELEASE'
+        controller = self.scene.getController(self.gamedata)
+        controller.handle(
+            self.gamedata,
+            events.Event(
+                kind,
+                {
+                    'key': key,
+                },
+                self.wnd,
+            ),
+        )
+
+    def setupShaderInvocation(self):
+        controller = self.scene.getController(self.gamedata)
+        updates = controller.shaderUpdates(self.gamedata)
+        for k, v in updates.items():
+            self.prog[k].value = v
+        newGeometryState = self.scene.geometryState(self.gamedata)
+        if self.geometryState != newGeometryState:
+            self.vertBuf.write(
+                self.scene.buildGeometry(newGeometryState)
+            )
+            self.geometryState = newGeometryState
+
+    def frame(self):
+        self.scene.getController().frame(self.gamedata)
+        if GameWindow.frame_callback != None:
+            (GameWindow.frame_callback)()
+
+def main(load_hook=None):
+    if load_hook != None:
+        load_hook(GameWindow)
+    mglw.run_window_config(GameWindow, args=('--window', 'tk'))
+
+if __name__ == '__main__':
+    main()
