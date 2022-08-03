c6d4297252b2b35f312ee3bd4b2bad3c19ab9187
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-03 14:12:21 -0400

    remove unused fn thingy

--- mglw_main.py
+++ mglw_main.py
@@ -35,19 +35,6 @@ class VertBufRef(object):
             return ctx.buffer(reserve=desiredSize)
         return inner
 
-def triNormal(tri):
-    a1 = tri[3] - tri[0]
-    a2 = tri[4] - tri[1]
-    a3 = tri[5] - tri[2]
-    b1 = tri[6] - tri[0]
-    b2 = tri[7] - tri[1]
-    b3 = tri[8] - tri[2]
-    return (
-        (a2*b3) - (a3*b2),
-        (a3*b1) - (a1*b3),
-        (a1*b2) - (a2*b1),
-    )
-
 class GameWindow(mglw.WindowConfig):
     title = 'Unnamed Window'
     gl_version = (3, 3)
