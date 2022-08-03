72438f373aab21e0fb80144e8fb30d2e06611fe5
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-03 14:14:38 -0400

    move `VertBufRef`

---
+++ vbo_utils.py
@@ -0,0 +1,27 @@
+class VertBufRef(object):
+    __slots__ = 'buf', 'size'
+
+    def __init__(self, size, alloc_fn=None):
+        self.buf = alloc_fn(size)
+        self.size = size
+
+    def reset(self, newData, alloc_fn=None, alloc_hook=None):
+        if alloc_fn == None:
+            raise Exception(
+                'VertBufRef.reset needs an alloc_fn specified'
+                + ' (it does not have an OpenGL/moderngl context'
+            )
+        self.buf.release()
+        newSize = len(newData.tobytes())
+        if self.size != newSize:
+            self.buf = alloc_fn(newSize)
+            self.size = newSize
+            if alloc_hook != None:
+                alloc_hook()
+        self.buf.write(newData)
+        
+    @classmethod
+    def makeAllocFn(cls, ctx):
+        def inner(desiredSize):
+            return ctx.buffer(reserve=desiredSize)
+        return inner
