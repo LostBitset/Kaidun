0215ab975e165870e20e85498a0191dcc5172671
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-04 11:53:07 -0400

    Getting there (this is so sketchy and awful i love it)

--- copy.py
+++ copy.py
@@ -1,17 +1,13 @@
 import mglw_main
 
 import inspect
-import subprocess 
 
 def overridenTk():
-    import tkinter
-    return tkinter.Tk()
-    # mglwRoot = mglw_main.startTk()
-    # return mglwRoot
+    mglwRoot = mglw_main.startTk()
+    return mglwRoot
 
 caller = inspect.currentframe().f_back
 for _ in range(5):
     caller = caller.f_back
 
 caller.f_globals['Tk'] = overridenTk
-
--- mglw_tk.py
+++ mglw_tk.py
@@ -2,10 +2,21 @@ import threading
 
 import mglw_main
 
+class MglwTkSingleton(object):
+    mglwSingletonRoot = None
+
+def mglwTkInitCallback(cfg):
+    MglwTkSingleton.mglwSingletonRoot = cfg.wnd._tk
+
+def mglwTkLoadHook(wincls):
+    wincls.init_callback = mglwTkInitCallback
+
 def startTk():
     load_hook = mglwTkLoadHook
     target = lambda: \
         mglw_main.main(load_hook=load_hook)
     thread = threading.Thread(target=target, args=())
     thread.start()
-
+    while MglwTkSingleton.mglwSingletonRoot == None:
+        pass
+    return MglwTkSingleton.mglwSingletonRoot
