9edae2da10f5b112beb7b7c5b383f415ae30072c
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-05 13:14:43 -0400

    access tris in `SceneController.frame`

--- scene_controllers.py
+++ scene_controllers.py
@@ -7,7 +7,7 @@ class SceneController(abc.ABC):
         return dict()
 
     @classmethod
-    def frame(cls, gamedata):
+    def frame(cls, gamedata, triangles):
         pass
 
     @classmethod
@@ -28,8 +28,8 @@ class MovingCamera(SceneController):
         }
 
     @classmethod
-    def frame(cls, gamedata):
-        super().frame(gamedata)
+    def frame(cls, gamedata, triangles):
+        super().frame(gamedata, triangles)
         ctr, dctr = gamedata['cam_ctr'], gamedata['d_cam_ctr']
         rot, drot = gamedata['cam_rot'], gamedata['d_cam_rot']
         gamedata['cam_ctr'] = (
