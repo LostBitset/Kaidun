16f5ea1425db7d776d30469bcc21648aba482a5d
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-07 12:11:35 -0400

    add `Geometry` objects

--- cpu_geom.py
+++ cpu_geom.py
@@ -1,5 +1,7 @@
 # Kaidun (by HktOverload)
 
+import numpy as np
+
 def triNormal(tri):
     a1 = tri[3] - tri[0]
     a2 = tri[4] - tri[1]
@@ -12,4 +14,32 @@ def triNormal(tri):
         (a3*b1) - (a1*b3),
         (a1*b2) - (a2*b1),
     )
+
+class Geometry(object):
+    __slots__ = ('tris',)
+
+    def __init__(self, tris):
+        self.tris = tris
+
+    def __or__(self, other):
+        return Geometry(
+            np.vstack((
+                self.tris,
+                other.tris,
+            ))
+        )
+
+    def place(self, loc):
+        L = []
+        for tri in self.tris:
+            normal = triNormal(tri)
+            for i in range(0, 9, 3):
+                coord = tri[i:(i + 3)]
+                L.extend(
+                    coord[0] + loc[0],
+                    coord[1] + loc[1],
+                    coord[2] + loc[2],
+                )
+                L.extend(normal)
+        return np.array(L, dtype='f4')
 
