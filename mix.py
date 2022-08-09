14c106e4ab3780e992ffc2c17861a7e2cba5f0bb
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-09 14:46:51 -0400

    finally

--- mix.py
+++ mix.py
@@ -30,50 +30,24 @@ class Mixin(object):
                         raise Exception(
                             f'Duplicate strategy for {name}.'
                         )
-                print(f'{name} <- (strat) {other._strats[name]}')
-                print(f'{name} <- (source) {other._sources[name]}')
                 newStrats[name] = other._strats[name]
                 newSources[name] = other._sources[name]
         newItems = dict()
-        print(f'newStrats = {newStrats}')
         for name, strat in newStrats.items():
-            print(f'strat={strat}')
             for other in others:
                 if not hasattr(other, name):
                     continue
                 item = getattr(other, name)
                 if name in newItems:
                     old = newItems[name]
-                    print(f'using (old) {old}')
                     mixed = strat(old, item)
                     newItems[name] = mixed
                 else:
-                    print(f'using {item}')
                     newItems[name] = item
-        print('items =', newItems)
-        print('strats =', newStrats)
-        print('sources =', newSources)
         return Mixin(
             None,
             newItems,
             newStrats,
             newSources,
         )
-
-    def _oldVer_use(*others):
-        self = Mixin('mixed')
-        strats, sources = self._strats, self._sources
-        for other in others:
-            for name, strat in other._strats.items():
-                if name not in strats:
-                    strats[name] = strat
-                    print(other._sources[name])
-                    sources[name] = other._sources[name]
-                elif sources[name] != other._sources[name]:
-                    print(sources[name], other._sources[name])
-                    raise Exception(
-                        f'Duplicate strategy {repr(strat)} for {name}.'
-                    )
-        print(f'strats={strats}')
-        print(f'sources={sources}')
 
--- mix.test.py
+++ mix.test.py
@@ -9,16 +9,13 @@ test1 = Mixin('test1', {'a': 1}, {'a': op.add})
 assert 'a' in test1._strats
 assert test1.a == 1
 
-base = Mixin('base', {'a': 9000}, {'a': op.add})
+base = Mixin('base', {'a': 0}, {'a': op.add})
 hi = Mixin('hi', {'a': 55}).use(base)
-print('---')
 ye = Mixin('ye', {'a': 5}).use(base)
-print('---')
 
 combined = Mixin().use(hi, ye)
 
-print(combined.a)
-assert combined.a == 9060
+assert combined.a == 60
 
 print('Passed!')
 
