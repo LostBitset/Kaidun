ec8e566a69945941c5629ed978434a18fef5d185
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-09 18:27:56 -0400

    basic graph stuff

--- graphs.py
+++ graphs.py
@@ -12,3 +12,44 @@ class Edge(object):
         suffix = ')'
         return f'{prefix}{sep.join(self.pts)}{suffix}'
 
+# A graph represented as an adjacency list
+class Graph(object):
+    __slots__ = ('adjDict',)
+
+    def __init__(self):
+        self.adjDict = dict()
+
+    def edges(self):
+        return EdgesIterator(self)
+
+    def nodes(self):
+        return NodesIterator(self)
+
+    def addNode(self, newNode):
+        self.adjDict[newNode] = set()
+
+    def addEdge(self, newEdge):
+        for pt in newEdge.pts:
+            self.adjDict[pt] = newEdge
+
+class EdgesIterator(object):
+    __slots__ = ('graph',)
+
+    def __init__(self, graph):
+        self.graph = graph
+
+    def __iter__(self):
+        for edgeSet in self.graph.adjDict.values():
+            for edge in edgeSet:
+                yield edge
+
+class NodesIterator(object):
+    __slots__ = ('graph',)
+
+    def __init__(self, graph):
+        self.graph = graph
+
+    def __iter__(self):
+        for node in self.graph.adjDict:
+            yield node
+
