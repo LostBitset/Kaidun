8590e492b20323f9d9baafdb515be86430d55f80
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-10 14:54:36 -0400

    test graph

--- terrain.py
+++ terrain.py
@@ -1,8 +1,15 @@
 # Kaidun (by HktOverload)
 
 import graphs
-import terrain_graphs
 
-def genGraph():
-    a = graphs.Graph()
+def genTestGraph():
+    res = graphs.Graph()
+    a, b, c = (0.0, 0.0), (10.0, -2.0), (2.0, -8.0)
+    for i in (a, b, c):
+        res.addNode(i)
+    edgecls = graphs.DirectedEdgeIn2D
+    res.addEdge(edgecls(a, b))
+    res.addEdge(edgecls(b, c))
+    res.addEdge(edgecls(c, a))
+    return res
 
