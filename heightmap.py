7754a88ed518688104b1e7b7ecfea6b3fd3a0dd5
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-11 18:33:04 -0400

    heightmap framework appears to be working

--- heightmap.py
+++ heightmap.py
@@ -1,13 +1,23 @@
 # Kaidun (by HktOverload)
 
 class Heightmap(object):
-    __slots__ = ('edges',)  # A list of EdgeIn2D objects
+    __slots__ = ('edges', 'memo')  # A list of EdgeIn2D objects
 
     def __init__(self, edges):
         self.edges = edges
+        self.memo = dict()
 
     def get(self, pos):
-        return -1.0
+        if pos in self.memo:
+            return self.memo[pos]
+        else:
+            res = self.getValue(pos)
+            res -= 2.0
+            self.memo[pos] = res
+            return res
+
+    def getValue(self, pos):
+        return 1.0
 
 def fromGraph(graph):
     allEdges = set()
