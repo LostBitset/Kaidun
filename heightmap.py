8bf58cdb1886e51c80ceaf09309a1ab0b7cc99f1
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-11 18:22:44 -0400

    back to work

--- heightmap.py
+++ heightmap.py
@@ -1,5 +1,17 @@
 # Kaidun (by HktOverload)
 
-def fromGraph(_):
-    return NotImplemented
+class Heightmap(object):
+    __slots__ = ('edges',)  # A list of EdgeIn2D objects
+
+    def __init__(self, edges):
+        self.edges = edges
+
+    def get(self, pos):
+        raise NotImplementedError
+
+def fromGraph(graph):
+    allEdges = set()
+    for edges in graph.adjDict.values():
+        allEdges.update(edges)
+    return Heightmap(list(allEdges))
 
