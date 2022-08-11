0cf8e61acd1dd138331681a0598c0d2879f0cddf
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-11 09:53:52 -0400

    tacos

--- delaunay.py
+++ delaunay.py
@@ -21,7 +21,21 @@ class Triangulation(object):
         self.counter = 0  # Keep track of IDs
 
     def addTri(self, tri):
+        ids = []
         for vert in tri:
-            if vert not in self.vert:
+            if vert not in self.verts:
+                self.verts[vert] = self.counter
+                self.counter += 1
+            ids.append(self.verts[vert])
+        self.refs.add(tuple(ids))
 
+    def dropTri(self, tri):
+        ids = [ self.verts[vert] for vert in tri ]
+        self.refs.discard(ids)
+
+    def dropVertex(self, vert):
+        badID = self.verts.pop(vert)
+        for tri in self.refs:
+            if badID in tri:
+                self.refs.discard(tri)
 
