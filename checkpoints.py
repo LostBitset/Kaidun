9ed31f97addd1b8c82ce70218d93dd361e774c99
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-12 11:52:41 -0400

    checkpoint generation seems to be working

--- checkpoints.py
+++ checkpoints.py
@@ -1,5 +1,7 @@
 # Kaidun (by HktOverload)
 
+import numpy as np
+
 from math import copysign
 import random
 
@@ -7,16 +9,21 @@ from terrain_graph_utils import dist
 
 class Checkpoints(object):
     __slots__ = ('items',)
-    count = 3
+    count = 10
 
     def __init__(self, items):
         self.items = items
 
+    def __iter__(self):
+        for checkpoint in self.items:
+            yield checkpoint
+
     @classmethod
-    def selectForEdge(cls, edge, numSteps=10):
+    def selectForEdge(cls, edge):
+        numSteps = 10 * cls.count
         step = 1. / numSteps
         stepDistance = dist(edge.src, edge.dst)
-        radsPerMeter = 1.0
+        radsPerMeter = 0.05
         stepRoll = stepDistance * radsPerMeter
         roll = 0.0
         indices = {
@@ -25,14 +32,19 @@ class Checkpoints(object):
         }
         L = []
         for i in range(numSteps):
+            signSource = random.random() - 0.5
+            roll += copysign(stepRoll, signSource)
+            if abs(roll) > (2. * np.pi):
+                roll = copysign(0.4, signSource)
             if i in indices:
                 L.append(
                     Checkpoint(i * step, roll)
                 )
-            signSource = random.random() - 0.5
-            roll += copysign(stepRoll, signSource)
         res = cls(L)
-        print(f'~> new checkpoints: {res}')
+        print('--- new checkpoints ---')
+        for checkpoint in res:
+            print(f'| ~> {checkpoint}')
+        print('--- end checkpoints ---')
         return res
 
 class Checkpoint(object):
@@ -41,4 +53,8 @@ class Checkpoint(object):
     def __init__(self, t, roll):
         self.t = t
         self.roll = roll
+
+    def __repr__(self):
+        c = self.__class__.__name__
+        return f'{c}(t={self.t}, roll={self.roll})'
 
