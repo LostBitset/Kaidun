a3914305d0c70e31f9fa1050f59261905f6e8d4d
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-08-12 01:07:27 -0400

    awesome

--- false_valley_checker.py
+++ false_valley_checker.py
@@ -2,7 +2,7 @@
 
 import terrain_graph_utils as u
 
-def ok(triangulation, thresh=4.0):
+def ok(triangulation, thresh=2.0):
     for tri in triangulation:
         incenter = u.incenter(tri)
         localMinDist = u.minSideDist(tri, incenter)
